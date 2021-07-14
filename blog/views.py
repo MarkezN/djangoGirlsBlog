bb747c9eff9d7274ae6f9228ed065c76833e19a6
marquez <marknovak85@gmail.com>
2021-07-14 12:39:09 +0200

    modified template

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
 
 # Create your views here.
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(datum_objave__lte=timezone.now()).order_by('datum_objave')
+    return render(request, 'blog/post_list.html', {'posts' : posts})
